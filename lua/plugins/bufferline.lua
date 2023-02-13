f6c4ac831ad4d1b0c38b5e7ae544878254ba7c48
Thungghuan <1092887426@qq.com>
2023-02-13 10:53:42 +0800

    feat: update bufferline appearance

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,6 +2,14 @@ local bufferline = require("bufferline")
 
 bufferline.setup({
   options = {
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { 'close' },
+    },
+    indicator = {
+      style = 'underline',
+    },
     offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1, } },
   }
 })
